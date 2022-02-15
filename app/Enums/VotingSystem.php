3bfd2d4f15b7ed8de16dd17244188c5c621f5dec
Armend Murtishi <armend@armend.cc>
2022-02-15 00:59:04 -0500

    Build Eloquent models for each table, and define relationship functions.

---
+++ app/Enums/VotingSystem.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Enums;
+
+enum VotingSystem: string
+{
+    case FPTP = 'fptp';
+    case IRV; = 'irv';
+}
--- app/Models/Candidate.php
+++ app/Models/Candidate.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Candidate extends Model
 {
     use HasFactory;
+
+    public function election()
+    {
+        return $this->belongsTo(Election::class);
+    }
 }
