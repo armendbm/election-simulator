f8642c1935f17fb78511cfe9a06f8fa2ac92c26a
Albert Dong <albertddong@hotmail.com>
2022-02-25 18:29:32 -0500

    Added template for choosing candidates on voting screen

--- resources/views/votingscreen.blade.php
+++ resources/views/votingscreen.blade.php
@@ -1,54 +1,30 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Voting Screen') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    {{ auth()->user()->name }}'s Information
-                </div>
-                <div class="p-6 bg-white ">
-                    Id: {{ auth()->user()->id }}
-                </div>
-                <div class="p-6 bg-white ">
-                    Username: {{ auth()->user()->name }}
-                </div>
-                <div class="p-6 bg-white ">
-                    Email: {{ auth()->user()->email }}
-                </div>
-                <div class="p-6 bg-white ">    
-                    Password: {{ auth()->user()->password }}
-                </div>
-                <div class="p-6 bg-white ">    
-                    Account created: {{ auth()->user()->created_at }}
-                </div>
-                <div class="p-6 bg-white bg-white border-b border-gray-200">    
-                    Last Updated: {{ auth()->user()->updated_at }}
-                </div>
-
-                <div class="float-right mt-4 ">
-                    <x-button class="ml-4 mb-4 bg-primary">
-                        <a href="update/{{ auth()->user()->id }}">
-                            {{ __('Update Info') }}
-                        </a>
-                    </x-button>
-
-                    <x-button class="ml-4 mb-4 bg-primary">
-                        <a href="updatepassword/{{ auth()->user()->id }}">
-                            {{ __('Update Password') }}
-                        </a>
-                    </x-button>
-    
-                    <x-button id="click" class="ml-4 mb-4 bg-danger">
-                        <a href="delete/{{ auth()->user()->id }}">
-                            Delete    
-                        </a>
-                    </x-button>
-                </div>    
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg text-center">
+                <form>
+                    <p style="padding: 1rem;">Select Candidate: (Template)</p>
+                    <div style="padding: 1.5rem;">
+                        <input type="radio" name="candidate" id="one" value="One" />
+                        <label for="one">Candidate 1</label><br>
+                    </div>
+                    <div style="padding: 1.5rem;">
+                        <input type="radio" name="candidate" id="two" value="Two" />
+                        <label for="two">Candidate 2</label><br>
+                    </div>
+                    <div style="padding: 1.5rem;">
+                        <input type="radio" name="candidate" id="three" value="Three" />
+                        <label for="three">Candidate 3</label><br>
+                    </div>
+                    <button type="submit" class="bg-green-500 
+                    shadow-5xl mb-10 p-2 w-80 uppercase font-bold">Submit</button>
+                </form>
             </div>
         </div>
     </div>
