5762b0a6dd883f6d5def473a3f951d1aeff5eb89
Armend Murtishi <armend@armend.cc>
2022-03-17 16:52:33 -0400

    Finalize changes to app.blade.php, making the footer automatically go to the bottom.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,32 +1,36 @@
 <!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+<html lang="en" class="h-100">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <!-- Stylesheets -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
-    <!-- Scripts -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous" defer></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous" defer></script>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11" defer></script>
-    <script src="{{ asset('js/app.js') }}" defer></script>
+        <!-- Stylesheets -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+        <!-- Scripts -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous" defer></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous" defer></script>
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11" defer></script>
+        <script src="{{ asset('js/app.js') }}" defer></script>
 
-    <title>{{ config('app.name', 'Elections To Go') }}</title>
-  </head>
-  <body>
-    <header class="site-header sticky-top">
-        @include('layouts.navigation')
-    </header>
-    <main>
-        {{ $slot }}
-    </main>
-    <footer class="py-3" style="background-color: #f9f9f9; text-align: center">
-        Copyright &copy; 2022 Albert Dong, Po Jui Juan, Parker McCormick, Armend Murtishi, and Noah Pedroso. All rights reserved.
-    </footer>
-    @include('sweetalert::alert')
-  </body>
+        <title>{{ config('app.name', 'Elections To Go') }}</title>
+    </head>
+    <body class="d-flex flex-column h-100">
+        <header class="site-header sticky-top">
+            @include('layouts.navigation')
+        </header>
+        <main>
+            <div class="container">
+                {{ $slot }}
+            </div>
+        </main>
+        <footer class="mt-auto py-3 bg-light">
+            <div class="container text-center">
+                <span class="text-muted">Copyright &copy; 2022 Albert Dong, Po Jui Juan, Parker McCormick, Armend Murtishi, and Noah Pedroso. All rights reserved.</span>
+            </div>
+        </footer>
+        @include('sweetalert::alert')
+    </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <div class="position-relative p-3 p-md-5 m-md-3 text-center">
+    <div class="p-3 p-md-5 m-md-3 text-center">
         <div class="col-md-5 p-lg-5 mx-auto my-5">
             <h1>Elections To-Go</h1>
             <p class="lead">An election simulator that teaches you about democracy.</p>
