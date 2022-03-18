ad3f41f054f740090bf638fba6658f1c4bdefbd3
Armend Murtishi <armend@armend.cc>
2022-03-18 00:26:14 -0400

    Remove all Tailwind remnants from site pages, and fix the buttons to use Bootstrap style.

--- resources/views/votingscreen.blade.php
+++ resources/views/votingscreen.blade.php
@@ -1,9 +1,7 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Voting Screen') }}
-        </h2>
-    </x-slot>
+    <h2>
+        {{ __('Voting Screen') }}
+    </h2>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
