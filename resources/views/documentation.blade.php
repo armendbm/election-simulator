678ef10882410b02da5ce419203da03bee2d1399
Po <bray851011@gmail.com>
2022-04-08 13:30:11 -0400

    add accordion in documentation page

--- resources/views/documentation.blade.php
+++ resources/views/documentation.blade.php
@@ -16,41 +16,60 @@
                         There is no educational tool on the market which allows you to visually compare how different voting systems work, nor is there an effective tool to demonstrate how different  systems, like the Electoral College, works. For the remainder of this report, we will provide an overview of the development process, schedule, risks, features, and a business case to showcase the value of our product.
                     </p>
                     <br>
-                    <h4 class="mt-3">FPTP</h4>
-                    <p style="text-indent: 5rem">
-                        Standing for "first-past-the-post", the FPTP electoral
-                        system is the quintessential electoral system in which the
-                        candidate with the plurality of votes is the winner. That
-                        is just a fancy way of saying that whoever has the most votes
-                        wins, end of story.
-                    </p>
-                    <p style="text-indent: 5rem">
-                        FPTP is also known as single-member plurality voting (SMP) or
-                        choose-one voting.
-                        It is the primary electoral system in much of the English-speaking
-                        world, including Canada, India, the United Kingdom,
-                        and, most notably, the United States.
-                    </p>
-                    <br>
-                    <h4 class="mt-3">IRV</h4>
-                    <p style="text-indent: 5rem">
-                        Standing for "instant-runoff voting", the IRV electoral
-                        system is a form of ranked preferential voting in which voters
-                        rank the candidates from their most to least preferred. There
-                        will be rounds in which the candidates with the fewest
-                        first-perference votes will be eliminated, and each vote that
-                        had the eliminated candidate as the first preference will
-                        then be counted for the vote's candidate with the next
-                        highest preference. This cycle continues until the desired number of candidates
-                        remain whom will then be declared the winners.
-                    </p>
-                    <p style="text-indent: 5rem">
-                        IRV is sometimes referred to as preferential voting,
-                        alternative voting, ranked-choice voting (United States), or
-                        single transferable voting (New Zealad). It is practiced
-                        nationwide in Australia and Canada, and has been practiced
-                        on local levels in the United States.
-                    </p>
+                    <div class="accordion accordion-flush" id="accordionFlushExample">
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="flush-headingOne">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                                    <b>FPTP</b>
+                                </button>
+                            </h2>
+                            <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                                <br>
+                                <p style="text-indent: 5rem">
+                                    Standing for "first-past-the-post", the FPTP electoral
+                                    system is the quintessential electoral system in which the
+                                    candidate with the plurality of votes is the winner. That
+                                    is just a fancy way of saying that whoever has the most votes
+                                    wins, end of story.
+                                </p>     
+                                <p style="text-indent: 5rem">
+                                    FPTP is also known as single-member plurality voting (SMP) or
+                                    choose-one voting.
+                                    It is the primary electoral system in much of the English-speaking
+                                    world, including Canada, India, the United Kingdom,
+                                    and, most notably, the United States.
+                                </p>                     
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="flush-headingTwo">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                                    <b>IRV</b>
+                                </button>
+                            </h2>
+                            <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+                                <br>
+                                <p style="text-indent: 5rem">
+                                    Standing for "instant-runoff voting", the IRV electoral
+                                    system is a form of ranked preferential voting in which voters
+                                    rank the candidates from their most to least preferred. There
+                                    will be rounds in which the candidates with the fewest
+                                    first-perference votes will be eliminated, and each vote that
+                                    had the eliminated candidate as the first preference will
+                                    then be counted for the vote's candidate with the next
+                                    highest preference. This cycle continues until the desired number of candidates
+                                    remain whom will then be declared the winners.
+                                </p>
+                                <p style="text-indent: 5rem">
+                                    IRV is sometimes referred to as preferential voting,
+                                    alternative voting, ranked-choice voting (United States), or
+                                    single transferable voting (New Zealad). It is practiced
+                                    nationwide in Australia and Canada, and has been practiced
+                                    on local levels in the United States.
+                                </p>                            
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
