d760bb53e4e206ebd000cf40df88254ea05632d6
Parker McCormick <93555630+mccorp2@users.noreply.github.com>
2022-04-18 15:01:09 -0400

    hotfix pt4 im problematic

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,9 +12,8 @@
 
     <div class="text-center">
         <div>
-            <h1>VISUAL OVERHAUL TO BE RELEASED</h1>
-            <h2>Beta 1.0 Release</h2>
-            <h2>Next Release: Final Release 05/27/2022</h2>
+            <h1>Beta 1.0 Release</h1>
+            <h2>Next Release: Final Release 04/27/2022</h2>
         </div>
     </div>
     @if (count($elections->where('public', 1)) > 0)
